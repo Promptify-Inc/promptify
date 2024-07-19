b88d936449f3a1a225c68c488bc433c30a69bd73
magdielian-code <oziomaagaecheta81@gmail.com>
2024-07-19 18:01:56 +0100

    feat: build app

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import '@styles/globals.css';
+export const metadata = {
+    title: "Promptify",
+    description: 'An emporium of prompts'
+}
+
+export const layout = () => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+
+            </div>
+        </body>
+
+    </html>
+  )
+}
