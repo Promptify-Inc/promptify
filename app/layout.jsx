1262302fc94bba6ae0454145f11212a61e778de4
magdielian-code <oziomaagaecheta81@gmail.com>
2024-07-20 02:42:34 +0100

    feat: added components files and fixed page and layout file.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,26 @@
 import '@styles/globals.css';
+
+
 export const metadata = {
     title: "Promptify",
     description: 'An emporium of prompts'
-}
+};
 
-export const layout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
             <div className='main'>
-
+                <div className='gradient'/>
             </div>
+
+            <main className='app'>
+              {children}
+            </main>
         </body>
 
     </html>
   )
 }
+
+export default RootLayout;
