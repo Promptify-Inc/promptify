4aa8dc2d8292721a2dedaebb3bd33265d863a4ac
magdielian-code <oziomaagaecheta81@gmail.com>
2024-07-20 11:17:10 +0100

    feat: compiled to prettier

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,54 +2,53 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-        <Link  href="/" className="flex gap-2 flex-center">
-            <Image 
-                src="/assets/images/new-logo.png"
-                alt="Promptify Logo"
-                width={30}
-                height={30}
-                className="object-contain" 
-            />
-            <p className="logo_text">Promptify</p>
-        </Link>
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/new-logo.png"
+          alt="Promptify Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptify</p>
+      </Link>
 
-        {/* {Mobile Nvigtion} */}
-        <div className="sm:flex hidden">
-            {isUserLoggedIn ? (
-                <div className="flex gap-3 md:gap-5">
-                    <Link href="/create-prompt" className="black_btn">
-                        Creat Prompt
-                    </Link>
+      {/* {Mobile Nvigtion} */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Creat Prompt
+            </Link>
 
-                    <button type="button" onClick={signOut} className="outline_btn">
-                        Sign Out
-                    </button>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
 
-                    <Link href="/profile">
-                        <Image 
-                            src="/assets/images/logo.svg"
-                            width={37}
-                            height={37}
-                            className="rounded-full"
-                            alt="profile"
-                        />
-                    </Link>
-                </div>
-            ): (
-                <>
-                </>
-            )}
-        </div>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
