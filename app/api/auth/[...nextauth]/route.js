ae206acb1224bb8b34706bba597cbebe55f2c152
magdielian-code <oziomaagaecheta81@gmail.com>
2024-07-20 20:47:23 +0100

    feat: started fix for mongodb and api routes.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,23 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
 
     },
     async signIn({ profile }) {
+        try {
+            
+        } catch (error) {
 
+        }
     }
 })
 
---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+let isConnected = false; // track the connection
+
+export const connecToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "promptifydB",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true;
+
+        console.log('MOngoDB connected')
+    } catch (error) {
+        console.log("Hey there! It's not your fault, here's the reason below");
+        console.log(error);
+    }
+}
